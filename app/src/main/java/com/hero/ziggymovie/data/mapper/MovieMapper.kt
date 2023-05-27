65ae7e55c567ab5ac6166f78550dc6d6bcaf6d9b
thsamajiki <ant1102@naver.com>
2023-05-27 23:49:25 +0900

    TMDB API로 교체

--- app/src/main/java/com/hero/ziggymovie/data/mapper/MovieMapper.kt
+++ app/src/main/java/com/hero/ziggymovie/data/mapper/MovieMapper.kt
@@ -1,18 +1,31 @@
 package com.hero.ziggymovie.data.mapper
 
-import com.hero.ziggymovie.data.model.MovieResponse
 import com.hero.ziggymovie.data.model.Movie
+import com.hero.ziggymovie.data.model.MovieResponse
 
+private const val IMAGE_DOMAIN = "https://image.tmdb.org/t/p/original"
 
 fun MovieResponse.toEntity(): Movie {
     return Movie(
-        title = title,
-        subtitle = subtitle,
-        webLink = link,
-        imageUrl = image,
-        publishedYear = pubDate,
-        directors = directors.split("|").filter { it.isNotEmpty() },
-        actors = actors.split("|").filter { it.isNotEmpty() },
-        userRating = userRating.toFloatOrNull() ?: 0f
+        id = id,
+        title = title.orEmpty(),
+        overView = overview.orEmpty(),
+        poster = IMAGE_DOMAIN + posterPath.orEmpty(),
+        releaseDate = releaseDate.orEmpty(),
+        voteAverage = voteAverage
     )
 }
+
+
+//fun MovieResponse.toEntity(): Movie {
+//    return Movie(
+//        title = title,
+//        subtitle = subtitle,
+//        webLink = link,
+//        imageUrl = image,
+//        publishedYear = pubDate,
+//        directors = directors.split("|").filter { it.isNotEmpty() },
+//        actors = actors.split("|").filter { it.isNotEmpty() },
+//        userRating = userRating.toFloatOrNull() ?: 0f
+//    )
+//}
