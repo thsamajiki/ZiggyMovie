cf97e73fd3e6fdf7ed00053df86f0a2bbf726ab4
thsamajiki <ant1102@naver.com>
2023-05-27 23:48:54 +0900

    TMDB API로 교체

--- app/src/main/java/com/hero/ziggymovie/data/model/MovieResponse.kt
+++ app/src/main/java/com/hero/ziggymovie/data/model/MovieResponse.kt
@@ -3,20 +3,32 @@ package com.hero.ziggymovie.data.model
 import com.google.gson.annotations.SerializedName
 
 data class MovieResponse(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("adult")
+    val adult: Boolean,
+    @SerializedName("backdrop_path")
+    val backdropPath: String?,
+    @SerializedName("genre_ids")
+    val genreIds: List<Int>?,
+    @SerializedName("original_language")
+    val originalLanguage: String?,
+    @SerializedName("original_title")
+    val originalTitle: String?,
+    @SerializedName("overview")
+    val overview: String?,
+    @SerializedName("popularity")
+    val popularity: Double,
+    @SerializedName("poster_path")
+    val posterPath: String?,
+    @SerializedName("release_date")
+    val releaseDate: String?,
     @SerializedName("title")
-    val title: String,
-    @SerializedName("link")
-    val link: String,
-    @SerializedName("image")
-    val image: String,
-    @SerializedName("subtitle")
-    val subtitle: String,
-    @SerializedName("pubDate")
-    val pubDate: String,
-    @SerializedName("director")
-    val directors: String,
-    @SerializedName("actor")
-    val actors: String,
-    @SerializedName("userRating")
-    val userRating: String
+    val title: String?,
+    @SerializedName("video")
+    val video: Boolean,
+    @SerializedName("vote_average")
+    val voteAverage: Double,
+    @SerializedName("vote_count")
+    val voteCount: Int
 )
