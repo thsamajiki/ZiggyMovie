530cf207b434763075ad5e786f8c34643b5a74ea
thsamajiki <ant1102@naver.com>
2023-05-27 23:50:40 +0900

    TMDB API로 교체

--- app/src/main/java/com/hero/ziggymovie/data/model/Movie.kt
+++ app/src/main/java/com/hero/ziggymovie/data/model/Movie.kt
@@ -5,12 +5,23 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Movie(
+    val id: Int,
     val title: String,
-    val subtitle: String,
-    val webLink: String,
-    val imageUrl: String,
-    val publishedYear: String,
-    val directors: List<String>,
-    val actors: List<String>,
-    val userRating: Float
+    val overView: String,
+    val poster: String,
+    val releaseDate : String,
+    val voteAverage: Double
 ) : Parcelable
+
+
+//@Parcelize
+//data class Movie(
+//    val title: String,
+//    val subtitle: String,
+//    val webLink: String,
+//    val imageUrl: String,
+//    val publishedYear: String,
+//    val directors: List<String>,
+//    val actors: List<String>,
+//    val userRating: Float
+//) : Parcelable
