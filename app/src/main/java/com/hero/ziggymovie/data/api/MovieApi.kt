03aeb8cf202d458c33839ac0cfafa90405da467a
thsamajiki <ant1102@naver.com>
2023-05-27 23:48:18 +0900

    TMDB API로 교체

--- app/src/main/java/com/hero/ziggymovie/data/api/MovieApi.kt
+++ app/src/main/java/com/hero/ziggymovie/data/api/MovieApi.kt
@@ -3,23 +3,19 @@ package com.hero.ziggymovie.data.api
 import com.hero.ziggymovie.data.model.MovieListResponse
 import io.reactivex.Single
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface MovieApi {
 
-    @GET("/v1/search/movie.json")
+    @GET("movie/popular")
     fun getMovieList(
-        @Header("X-Naver-Client-Id") clientId: String,
-        @Header("X-Naver-Client-Secret") clientSecret: String,
-        @Query("query") keyword: String,
-        @Query("start") start: Int,
-        @Query("display") display: Int = 10
+        @Query("page") page : Int,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language : String = "ko"
     ) : Single<MovieListResponse>
 
     companion object {
-        const val BASE_URL = "https://openapi.naver.com"
-        const val CLIENT_ID = "r2_QGtDthxd3rfdGTBL9"
-        const val CLIENT_SECRET_KEY = "9SjkOsayZ3"
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        private const val API_KEY = "d99bfebe6e692708ef648145bc997704"
     }
 }
