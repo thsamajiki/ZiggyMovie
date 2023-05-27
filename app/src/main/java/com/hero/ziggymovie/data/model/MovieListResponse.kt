854c632ecdeac6f842ad7244c0159402d443d66d
thsamajiki <ant1102@naver.com>
2023-05-27 23:49:10 +0900

    TMDB API로 교체

--- app/src/main/java/com/hero/ziggymovie/data/model/MovieListResponse.kt
+++ app/src/main/java/com/hero/ziggymovie/data/model/MovieListResponse.kt
@@ -3,15 +3,8 @@ package com.hero.ziggymovie.data.model
 import com.google.gson.annotations.SerializedName
 
 data class MovieListResponse(
-    @SerializedName("lastBuildDate")
-    val lastBuildDate: String,
-    @SerializedName("total")
-    val total: Int,
-    @SerializedName("start")
-    val start: Int,
-    @SerializedName("display")
-    val display: Int,
-    @SerializedName("items")
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
     val movieList: List<MovieResponse>
-) {
-}
+)
