8ad3405c91f526221b7dbb3303e8d27d176ef03c
thsamajiki <ant1102@naver.com>
2023-05-30 11:53:05 +0900

    영화 목록 API Response 수정

--- app/src/main/java/com/hero/ziggymovie/data/mapper/MovieMapper.kt
+++ app/src/main/java/com/hero/ziggymovie/data/mapper/MovieMapper.kt
@@ -1,31 +1,25 @@
 package com.hero.ziggymovie.data.mapper
 
-import com.hero.ziggymovie.data.model.Movie
+import com.hero.ziggymovie.data.model.MovieData
 import com.hero.ziggymovie.data.model.MovieResponse
 
 private const val IMAGE_DOMAIN = "https://image.tmdb.org/t/p/original"
 
-fun MovieResponse.toEntity(): Movie {
-    return Movie(
+fun MovieResponse.toEntity(): MovieData {
+    return MovieData(
         id = id,
-        title = title.orEmpty(),
-        overView = overview.orEmpty(),
-        poster = IMAGE_DOMAIN + posterPath.orEmpty(),
+        adult = adult,
+        backdropPath = backdropPath,
+        genreIds = genreIds,
+        originalLanguage = originalLanguage,
+        originalTitle = originalTitle,
+        overview = overview.orEmpty(),
+        popularity = popularity,
+        posterPath = IMAGE_DOMAIN + posterPath.orEmpty(),
         releaseDate = releaseDate.orEmpty(),
-        voteAverage = voteAverage
+        title = title.orEmpty(),
+        video = video,
+        voteAverage = voteAverage,
+        voteCount = voteCount
     )
 }
-
-
-//fun MovieResponse.toEntity(): Movie {
-//    return Movie(
-//        title = title,
-//        subtitle = subtitle,
-//        webLink = link,
-//        imageUrl = image,
-//        publishedYear = pubDate,
-//        directors = directors.split("|").filter { it.isNotEmpty() },
-//        actors = actors.split("|").filter { it.isNotEmpty() },
-//        userRating = userRating.toFloatOrNull() ?: 0f
-//    )
-//}
