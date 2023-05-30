8f028e3c9573725589133fda530546279c7438b3
thsamajiki <ant1102@naver.com>
2023-05-30 11:52:46 +0900

    영화 목록 API Response 수정

--- app/src/main/java/com/hero/ziggymovie/data/model/MovieListResponse.kt
+++ app/src/main/java/com/hero/ziggymovie/data/model/MovieListResponse.kt
@@ -6,5 +6,9 @@ data class MovieListResponse(
     @SerializedName("page")
     val page: Int,
     @SerializedName("results")
-    val movieList: List<MovieResponse>
+    val movieList: List<MovieResponse>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
 )
