6b782e1893ac9dda954b6604de9d37b862ce0845
thsamajiki <ant1102@naver.com>
2023-05-30 11:37:10 +0900

    영화 검색 API, 영화 상세 정보 조회 API 추가

--- app/src/main/java/com/hero/ziggymovie/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/hero/ziggymovie/data/repository/MovieRepositoryImpl.kt
@@ -6,16 +6,40 @@ import androidx.paging.PagingData
 import androidx.paging.map
 import androidx.paging.rxjava2.flowable
 import com.hero.ziggymovie.data.mapper.toEntity
-import com.hero.ziggymovie.data.model.Movie
+import com.hero.ziggymovie.data.model.MovieData
+import com.hero.ziggymovie.data.model.MovieResponse
+import com.hero.ziggymovie.data.remote.MovieListPagingSource
 import com.hero.ziggymovie.data.remote.MoviePagingSource
 import com.hero.ziggymovie.data.remote.MovieRemoteDataSource
 import io.reactivex.Flowable
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class MovieRepositoryImpl @Inject constructor(
     private val remoteDataSource: MovieRemoteDataSource
 ): MovieRepository {
-    override fun getMovieList(page: Int, keyword: String): Flowable<PagingData<Movie>> {
+    override fun getMovieList(page: Int): Flow<PagingData<MovieData>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 12, // 각 페이지에 로드할 데이터 수
+                initialLoadSize = 12
+            ),
+            pagingSourceFactory = {
+                MovieListPagingSource(remoteDataSource) // PagingSource 인스턴스를 생성
+            }
+        )
+            .flow
+            .map {
+                it.map { movieResponse -> movieResponse.toEntity() }
+            }
+    }
+
+    override fun getMovieDetail(movieId: Int): MovieResponse {
+        return remoteDataSource.getMovieDetail(movieId)
+    }
+
+    override fun searchMovie(page: Int, keyword: String): Flowable<PagingData<MovieData>> {
         return Pager(
             config = PagingConfig(
                 pageSize = 12, // 각 페이지에 로드할 데이터 수
