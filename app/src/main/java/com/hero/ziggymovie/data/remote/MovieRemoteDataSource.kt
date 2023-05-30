f58de17b6b6812dd0c6d56593e9e0c3b67b5a5db
thsamajiki <ant1102@naver.com>
2023-05-30 11:37:24 +0900

    영화 검색 API, 영화 상세 정보 조회 API 추가

--- app/src/main/java/com/hero/ziggymovie/data/remote/MovieRemoteDataSource.kt
+++ app/src/main/java/com/hero/ziggymovie/data/remote/MovieRemoteDataSource.kt
@@ -2,17 +2,31 @@ package com.hero.ziggymovie.data.remote
 
 import com.hero.ziggymovie.data.api.MovieApi
 import com.hero.ziggymovie.data.model.MovieListResponse
+import com.hero.ziggymovie.data.model.MovieResponse
+import com.hero.ziggymovie.data.model.SearchMovieResponse
 import io.reactivex.Single
 import javax.inject.Inject
 
 interface MovieRemoteDataSource {
-    fun getMovieList(page: Int, keyword: String): Single<MovieListResponse>
+    fun getMovieList(page: Int): MovieListResponse
+
+    fun getMovieDetail(movieId: Int): MovieResponse
+
+    fun searchMovie(page: Int, keyword: String) : Single<SearchMovieResponse>
 }
 
 class MovieRemoteDataSourceImpl @Inject constructor(
     private val movieApi: MovieApi
 ) : MovieRemoteDataSource {
-    override fun getMovieList(page: Int, keyword: String): Single<MovieListResponse> {
+    override fun getMovieList(page: Int): MovieListResponse {
         return movieApi.getMovieList(page)
+    }
+
+    override fun getMovieDetail(movieId: Int): MovieResponse {
+        return movieApi.getMovieDetail(movieId)
+    }
+
+    override fun searchMovie(page: Int, keyword: String): Single<SearchMovieResponse> {
+        return movieApi.searchMovie(page, keyword)
     }
 }
