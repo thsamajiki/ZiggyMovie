6c61fbbacf6d759bc075b52bef71bf9985c67eb2
thsamajiki <ant1102@naver.com>
2023-05-30 11:36:43 +0900

    영화 검색 API API 추가

--- app/src/main/java/com/hero/ziggymovie/data/repository/MovieRepository.kt
+++ app/src/main/java/com/hero/ziggymovie/data/repository/MovieRepository.kt
@@ -1,9 +1,15 @@
 package com.hero.ziggymovie.data.repository
 
 import androidx.paging.PagingData
-import com.hero.ziggymovie.data.model.Movie
+import com.hero.ziggymovie.data.model.MovieData
+import com.hero.ziggymovie.data.model.MovieResponse
 import io.reactivex.Flowable
+import kotlinx.coroutines.flow.Flow
 
 interface MovieRepository {
-    fun getMovieList(page: Int, keyword: String): Flowable<PagingData<Movie>>
+    fun getMovieList(page: Int): Flow<PagingData<MovieData>>
+
+    fun getMovieDetail(movieId: Int): MovieResponse
+
+    fun searchMovie(page: Int, keyword: String): Flowable<PagingData<MovieData>>
 }
