14fa88229ace0b16b08a0a287c08119810f62b08
thsamajiki <ant1102@naver.com>
2023-05-30 11:55:35 +0900

    생성자에 MovieRepository 추가

--- app/src/main/java/com/hero/ziggymovie/view/viewmodel/MovieDetailViewModel.kt
+++ app/src/main/java/com/hero/ziggymovie/view/viewmodel/MovieDetailViewModel.kt
@@ -2,18 +2,20 @@ package com.hero.ziggymovie.view.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.hero.ziggymovie.data.model.Movie
+import com.hero.ziggymovie.data.model.MovieData
+import com.hero.ziggymovie.data.repository.MovieRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MovieDetailViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
+    private val movieRepository: MovieRepository
 ) : ViewModel() {
 
     companion object {
         const val KEY_MOVIE = "movie"
     }
 
-    val movie: Movie = savedStateHandle.get<Movie>(KEY_MOVIE)!!
+    val movieData: MovieData = savedStateHandle.get<MovieData>(KEY_MOVIE)!!
 }
